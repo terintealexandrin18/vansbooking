19dc8cd05fa2404cbba415f650cdeb6827b143c2
Alex Terinte <terintealexandrin18@gmail.com>
2024-03-24 13:48:22 +0000

    Add icons to the services and align in the center

--- bookings/views.py
+++ bookings/views.py
@@ -4,9 +4,9 @@ from .models import HomePage
 # Create your views here.
 
 def home(request):
-    home_page = HomePage.objects.all().order_by('-updated_on').first()
+    home_pages = HomePage.objects.all().order_by('-updated_on')
     return render(
         request,
         "bookings/index.html",
-        {"home_page": home_page},
+        {"home_pages": home_pages},
     )
