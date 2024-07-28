1ea1804c84e8c3edc205d6bdb8ab35409679fef4
Alex Terinte <terintealexandrin18@gmail.com>
2024-07-28 19:24:16 +0000

    Added placeholders and contact number validation for contact app

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import ContactMessage
-
 
 class ContactForm(forms.ModelForm):
     """
@@ -14,7 +14,20 @@ class ContactForm(forms.ModelForm):
     **Template**
     :template:`contact/contact.html`
     """
+
+    def clean_contact_number(self):
+        contact_number = self.cleaned_data.get('contact_number')
+        if not contact_number.isdigit() or len(contact_number) != 11:
+            raise ValidationError('Enter a valid 11-digit contact number.')
+        return contact_number
+
     class Meta:
         model = ContactMessage
-        fields = ['first_name', 'last_name', 'contact_number',
-                  'email', 'message']
+        fields = ['first_name', 'last_name', 'contact_number', 'email', 'message']
+        widgets = {
+            'contact_number': forms.TextInput(attrs={'placeholder': '01234567891'}),
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'your.email@example.com'}),
+            'message': forms.Textarea(attrs={'placeholder': 'Your message...'}),
+        }
